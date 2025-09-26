4c6eca0d24de49ab0a0d9d0354eaeb8b7e4d7f00
julian <julitheriveri@gmail.com>
2025-09-26 15:44:21 -0500

    Se crean las variables donde se almacenarán los datos de registro, validaciones y flujos entre las dos pantallas.

--- app/src/main/java/com/poli/registerloginpetsocial/LoginActivity.kt
+++ app/src/main/java/com/poli/registerloginpetsocial/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.poli.registerloginpetsocial
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ open class LoginActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val registerButton = findViewById<Button>(R.id.registerButton)
+        registerButton.setOnClickListener {
+            val intentRegisterActivity = Intent(this, RegisterActivity::class.java)
+            startActivity(intentRegisterActivity)
         }
     }
 }
--- app/src/main/java/com/poli/registerloginpetsocial/RegisterActivity.kt
+++ app/src/main/java/com/poli/registerloginpetsocial/RegisterActivity.kt
@@ -1,6 +1,11 @@
 package com.poli.registerloginpetsocial
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +20,45 @@ class RegisterActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val backButton = findViewById<Button>(R.id.backButton)
+        val registerConfirmButton = findViewById<Button>(R.id.registerConfirmButton)
+        val nameUser = findViewById<EditText>(R.id.userName)
+        val userEmail = findViewById<EditText>(R.id.userEmail)
+        val userPassword = findViewById<EditText>(R.id.userPassword)
+        val userConfirmPassword = findViewById<EditText> (R.id.userConfirmPassword)
+
+        registerConfirmButton.setOnClickListener {
+            val nameValue = nameUser.text.toString().trim()
+            val emailValue = userEmail.text.toString().trim()
+            val passwordValue = userPassword.text.toString().trim()
+            val confirmValue = userConfirmPassword.text.toString().trim()
+
+            if (nameValue.isEmpty() || emailValue.isEmpty() || passwordValue.isEmpty() || confirmValue.isEmpty()){
+                Toast.makeText(this, "Debe Diligenciar todos los Campos.", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(emailValue).matches()){
+                Toast.makeText(this, "El correo ingresado NO es válido.", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            if (passwordValue != confirmValue){
+                Toast.makeText(this, "Las contraseñas no coinciden.", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            Toast.makeText(this, "Registro Exitoso!", Toast.LENGTH_LONG).show()
+            val successRegisterIntent = Intent(this, LoginActivity::class.java)
+            startActivity(successRegisterIntent)
+            finish()
+        }
+
+        backButton.setOnClickListener {
+            val backIntent = Intent(this, LoginActivity::class.java)
+            startActivity(backIntent)
+            finish()
         }
     }
 }
